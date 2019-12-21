44db1cc3418c812dcdfae7b15994291323359bfc
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-21 18:59:42 -0500

    implemented post in Register in app.py

--- Image_Classification_API/web/app.py
+++ Image_Classification_API/web/app.py
@@ -5,3 +5,24 @@ import bcrypt
 import requests
 import subprocess
 import json
+
+app = Flask(__name__)
+api =  Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.ImageRecognition
+users = db["Users"]
+
+class Register(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+
+        if USerExists(username):
+            retJson = {
+                "status" : 301,
+                "msg" : "Invalid Username"
+            }
+            return jsonify(retJson)
