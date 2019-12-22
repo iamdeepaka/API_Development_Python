72eff0d82d74ada520787fa21462105a222f2d2f
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-21 23:23:21 -0500

    added Refill class to app.py

--- Image_Classification_API/web/app.py
+++ Image_Classification_API/web/app.py
@@ -122,3 +122,33 @@ class Classify(Resource):
         })
 
         return retJson
+
+
+class Refill(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["admin_pwd"]
+        amount = postedData["amount"]
+
+        if not UserExist(username):
+            return jsonify(generateReturnDictionary(301, "Invalid Username"))
+
+        correct_pw = "abc456"
+
+        if not password == correct_pw:
+            return jsonify(generateReturnDictionary(304, "Invalid Administrator Password"))
+
+
+        users.update({
+            "Username": username
+        }, {
+            "$set":{
+                "Tokens":amount
+            }
+        })
+
+        return jsonify(generateReturnDictionary(200, "Refilled Successfully"))
+
+
