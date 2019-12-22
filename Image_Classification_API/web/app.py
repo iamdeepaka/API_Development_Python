bc5bdd3a8905a0ca2395c2a8cfb538921c0acb8e
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-21 19:26:45 -0500

    added classify class in app.py

--- Image_Classification_API/web/app.py
+++ Image_Classification_API/web/app.py
@@ -13,11 +13,13 @@ client = MongoClient("mongodb://db:27017")
 db = client.ImageRecognition
 users = db["Users"]
 
+
 def UserExist(username):
     if users.find({"Username":username}).count()==0:
         return False
     else:
         return True
+
 
 class Register(Resource):
     def post(self):
@@ -48,3 +50,42 @@ class Register(Resource):
         }
 
         return jsonify(retJson)
+
+
+class Classify(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+        url = postedData["url"]
+
+        retJson, error = verifyCredentials(username, password)
+        if error:
+            return jsonify(retJson)
+
+        tokens = users.find({"Username":username})[0]["Tokens"]
+
+        if tokens<=0:
+            return jsonify(generateReturnDictionary("303", "Not Enough Tokens!"))
+
+        req = requests.get(url)
+        retJson = {}
+
+        with open("temp.jpg","wb") as image_file:
+            image_file.write(req.content)
+            proc = subprocess.Popen("python classify_image.py --model_dir=. --image_file=./temp.jpg")
+            proc.communicate()[0]
+            proc.wait()
+            with open("text.txt") as results_dict:
+                retJson = json.load(results_dict)
+
+        users.update({
+            "Username":username
+        }, {
+            "$set":{
+                         "Tokens": tokens - 1
+                     }
+        })
+
+        return retJson
